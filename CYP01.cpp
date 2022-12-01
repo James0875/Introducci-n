168645ef89803df6e06306fed43d6b2d04f78c1d
James0875 <James.com>
2022-12-01 00:52:51 -0600

    sexto commit

---
+++ CYP01.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+
+	int pixeles_ancho, pixeles_alto, Xizquierda, Yizquierda, Xderecha, Yderecha;
+	float porcentajeX, porcentajeY, porcentaje_ancho, porcenjate_alto;
+
+	scanf_s("%i", &pixeles_ancho);
+	scanf_s("%i", &pixeles_alto);
+	scanf_s("%f", &porcentajeX);
+	scanf_s("%f", &porcentajeY);
+	scanf_s("%f", &porcentaje_ancho);
+	scanf_s("%f", &porcenjate_alto);
+
+	Xizquierda = porcentajeX * pixeles_ancho;
+	Yizquierda = porcentajeY * pixeles_alto;
+	Xderecha = pixeles_ancho * porcentaje_ancho + Xizquierda;
+	Yderecha = pixeles_alto * porcenjate_alto + Yizquierda;
+
+	printf_s("%i %i %5.2f %5.2f %5.2f %5.2f %i %i %i %i", pixeles_ancho, pixeles_alto, porcentajeX, porcentajeY, porcentaje_ancho, porcenjate_alto, Xizquierda, Yizquierda, Xderecha, Yderecha);
+
+
+}
